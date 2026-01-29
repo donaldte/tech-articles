128e113cb0ea51fa46cd7bdbcec9297a3aabbb9d
tiakouang <djoukevin1469@gmail.com>
2026-01-29 04:41:19 +0100

    fix(logo): Update logo size for better responsiveness across devices

---
+++ tech_articles/static/js/dashboard/index.js
@@ -0,0 +1,10 @@
+/**
+ * Dashboard JavaScript Entry Point
+ * Imports and initializes all dashboard-related scripts
+ *
+ * @author Runbookly
+ * @version 1.0.0
+ */
+
+// Note: Dashboard scripts will be added here as the dashboard is developed
+// This file serves as the entry point for all dashboard-related JavaScript
---
+++ tech_articles/static/js/home/index.js
@@ -0,0 +1,10 @@
+/**
+ * Home Page JavaScript Entry Point
+ * Imports and initializes all home-related scripts
+ *
+ * @author Runbookly
+ * @version 1.0.0
+ */
+
+// Note: Individual script files are loaded via <script> tags in templates
+// This file serves as documentation for the module structure
